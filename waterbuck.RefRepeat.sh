d9b9322c97eb8d97a7bdea9ebd1d60c9967512f8
XiWang <47152099+xiqtcacf@users.noreply.github.com>
2022-12-02 17:52:50 +0100

    Delete waterbuck.RefRepeat.sh

--- waterbuck.RefRepeat.sh
+++
@@ -1,5 +0,0 @@
-RepeatMasker  -pa 40  -frag 60000 -species Mammalia /home/users/xi/Waterbuck_project/Reference/DefassaWaterbuck.fasta \
-				      -dir /home/users/xi/Waterbuck_project/4.sites_filtering/1.repeats -xm -xsmall -gff \
-				      2>&1 > /home/users/xi/Waterbuck_project/4.sites_filtering/1.repeats/waterbuck.log
-
-
