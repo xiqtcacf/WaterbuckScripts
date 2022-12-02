f63a0aa655760b0beb53dfdaa48847710b24ee7c
XiWang <47152099+xiqtcacf@users.noreply.github.com>
2022-12-02 17:52:43 +0100

    Delete waterbuck.RefRepeat.getNONrepeatbed.sh

--- waterbuck.RefRepeat.getNONrepeatbed.sh
+++
@@ -1,1 +0,0 @@
-/home/users/xi/software/bedtools2/bin/bedtools subtract -a DefassaWaterbuck.Ref.scaffolds.all.bed -b DefassaWaterbuck.fasta.masked.repeat.bed > DefassaWaterbuck.fasta.masked.NOrepeat.bed
