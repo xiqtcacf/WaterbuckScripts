3cc7d894b9c45eff58cdc5762cfa505e2cfd6092
XiWang <47152099+xiqtcacf@users.noreply.github.com>
2022-12-02 17:39:09 +0100

    Add files via upload

---
+++ waterbuck.RefRepeat.getNONrepeatbed.sh
@@ -0,0 +1,1 @@
+/home/users/xi/software/bedtools2/bin/bedtools subtract -a DefassaWaterbuck.Ref.scaffolds.all.bed -b DefassaWaterbuck.fasta.masked.repeat.bed > DefassaWaterbuck.fasta.masked.NOrepeat.bed
