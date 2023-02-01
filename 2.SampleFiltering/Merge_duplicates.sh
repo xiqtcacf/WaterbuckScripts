64f1c1d22c7b48769c25bb4e96f3ddf5501bf261
XiWang <47152099+xiqtcacf@users.noreply.github.com>
2023-02-01 14:05:15 +0100

    Create Merge_duplicates.sh

---
+++ 2.SampleFiltering/Merge_duplicates.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+batch1=$1 ###merged bam name
+batch2=$2 ###first bam need to be merged
+batch3=$3 ###second bam need to be merged, and can be added more inds
+batch4=$4 ###reference name 
+
+export DIR=/home/users/xi/Waterbuck_project/7.merged_Samples
+cd $DIR
+
+input=/home/users/xi/Waterbuck_project/raw_bam/
+output=/home/users/xi/Waterbuck_project/7.merged_Samples/
+samtools=/home/users/xi/software/samtools-1.14/samtools
+ 
+###### merge duplicated bams into one
+$samtools merge $batch1.$batch4.bam $input/$batch2.$batch4.bam  $input/$batch3.$batch4.bam -@ 20
+
+###### rename and index
+$samtools reheader --command 'sed "s/\tSM:[0-9].../\tSM:javanicus_1607_1608/g"' $batch1.$batch4.bam > $batch1.$batch4.Reheader.bam
+$samtools index $output/$batch1.$batch4.Reheader.bam  
+
+###### checking depth of merged samples
+$samtools depth -a $batch1.$batch4.Reheader.bam > $batch1.$batch4.Reheader.bam.depth
+cat $batch1.$batch4.Reheader.bam.depth | awk '{sum+=$3} END { print "Average = ",sum/NR}' > $batch1.$batch4.Reheader.bam.depth.mappingCoverage
