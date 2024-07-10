24299dede64492d45e95bfe85f9e946d022d1e5e
XiWang <47152099+xiqtcacf@users.noreply.github.com>
2024-07-10 13:44:05 +0200

    Update 7.2.waterbuck.globalFst.angsd_pop.2dSFS.Fst.sh

--- 7.DiversityAndDivergence/7.2.waterbuck.globalFst.angsd_pop.2dSFS.Fst.sh
+++ 7.DiversityAndDivergence/7.2.waterbuck.globalFst.angsd_pop.2dSFS.Fst.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 batch=$1
-export DIR=/home/users/xi/Waterbuck_project/10.Fst/Defassa_anc
+export DIR=/home/wlk579/0.waterbuck/Fst_WaterC
 cd $DIR
 
-input=/home/users/xi/Waterbuck_project/10.Fst/Defassa_anc/input
-file1=/home/users/xi/Waterbuck_project/10.Fst/Defassa_anc/$batch.1
-file2=/home/users/xi/Waterbuck_project/10.Fst/Defassa_anc/$batch.2
+file1=/home/wlk579/0.waterbuck/Fst_WaterC/$batch.1
+file2=/home/wlk579/0.waterbuck/Fst_WaterC/$batch.2
 
-angsd=/home/users/xi/software/angsd/angsd
-realSFS=/home/users/xi/software/angsd/misc/realSFS
+angsd=/home/wlk579/Server_bos/apps/angsd/angsd
+realSFS=/home/wlk579/Server_bos/apps/angsd/misc/realSFS
+input=/home/wlk579/0.waterbuck/Fst_WaterC/Defanc.saf.perPop
 
-# Do 2dsfs from Defassa saf files, use it to estimate global fsts
+# Do 2dsfs from waterbuck saf files, use it to estimate global fsts
 while IFS= read -r line1 && IFS= read -r line2 <&3; do
 
-   $realSFS $input/$line1 $input/$line2 -P 30 > $line1.$line2.Defassaanc.2dsfs
-   $realSFS fst index -whichFst 1 $input/$line1 $input/$line2 -P 30 -sfs $line1.$line2.Defassaanc.2dsfs -fstout $line1.$line2.Defassaanc
-   $realSFS fst stats $line1.$line2.Defassaanc.fst.idx > $line1.$line2.Defassaanc.Fst
+   $realSFS $input/$line1 $input/$line2 -P 20 > $line1.$line2.Defanc.2dsfs
+   $realSFS fst index -whichFst 1 $input/$line1 $input/$line2 -P 20 -sfs $line1.$line2.Defanc.2dsfs -fstout $line1.$line2.Defanc
+   $realSFS fst stats $line1.$line2.Defanc.fst.idx > $line1.$line2.Defanc.Fst
 
 done < $file1 3< $file2
