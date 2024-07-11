ebd98909044bb38dc714d171e9adf00d07957e69
XiWang <47152099+xiqtcacf@users.noreply.github.com>
2024-07-11 17:46:23 +0200

    Update 3.1.2.Step2.waterbuck.RefRepeat.getNONrepeatbed.sh

--- 3.SitesFiltering/3.1.2.Step2.waterbuck.RefRepeat.getNONrepeatbed.sh
+++ 3.SitesFiltering/3.1.2.Step2.waterbuck.RefRepeat.getNONrepeatbed.sh
@@ -1,4 +1,4 @@
-#### get 'extSoftRepetFA.py' written by Xiaodong Liu in  
+#### get 'extSoftRepetFA.py' written by Xiaodong Liu
 
 python3 extSoftRepetFA.py Banteng_PlusMt.draft.fasta.masked > Banteng_PlusMt.draft.fasta.masked.repeat.bed
 /home/users/xi/software/bedtools2/bin/bedtools subtract -a DefassaWaterbuck.Ref.scaffolds.all.bed -b DefassaWaterbuck.fasta.masked.repeat.bed > DefassaWaterbuck.fasta.masked.NOrepeat.bed
